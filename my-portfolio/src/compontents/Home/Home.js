13f8f894e105240dc0fc4e14849c44e451ff8d7d
mahalakshmi-mariappan03 <mahalakshmimariappan03@gmail.com>
2024-12-22 20:15:18 +0530

    add pdf link Home.js

--- my-portfolio/src/compontents/Home/Home.js
+++ my-portfolio/src/compontents/Home/Home.js
@@ -12,7 +12,7 @@ function Home() {
         <h3>Hi, I am Mahalakshmi</h3>
         <h4>Full Stack Develo<span className="text">per</span></h4>
         <p>As an enthusiastic Front-end and Back-end Developer with a strong foundation in REACT, CSS, JAVASCRIPT, and MongoDB, I am passionate about creating intuitive user interfaces and eager to contribute my skills to a collaborative team.</p>
-        <a href="MM.pdf" target='_blank'>
+        <a href="https://drive.google.com/file/d/1JffxnAq9K4JkF_rD7uY_9FJnSyxflJ61/view?usp=drive_link" target='_blank'>
           <button type="button" className="btn-dwld">Download CV</button>
         </a>
         <Link to="project" smooth={true} duration={500}>
